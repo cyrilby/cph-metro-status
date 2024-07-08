b13398b1da2119b8a17255fd5fc145966ad8366e
Kiril <102741768+cyrilby@users.noreply.github.com>
2024-07-08 18:03:45 +0200

    Added Bash scripts used for Linux automation
    These do not contain the scheduled times but only the commands needed to activate the relevant environment and then run the relevant scripts

---
+++ automation/run_cph_scraper_data_collection.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "/home/admin/Documents/cph-metro-status/"
+source /home/admin/Documents/cph-metro-status/metropy/bin/activate
+python /home/admin/Documents/cph-metro-status/get_data_chrome_linux.py
