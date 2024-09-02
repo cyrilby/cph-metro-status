8d88e5e9e4b51d0d0fcb1053fb4d9d51e696ba8b
Kiril <102741768+cyrilby@users.noreply.github.com>
2024-09-02 11:38:11 +0200

    No longer running different scripts on Linux machine

--- automation/run_cph_scraper_data_cleaning.sh
+++ automation/run_cph_scraper_data_cleaning.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "/home/admin/Documents/cph-metro-status/"
 source /home/admin/Documents/cph-metro-status/metropy/bin/activate
-python /home/admin/Documents/cph-metro-status/summarize_data_linux.py
+python /home/admin/Documents/cph-metro-status/summarize_data.py
