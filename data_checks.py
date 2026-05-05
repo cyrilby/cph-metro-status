4ae68182e23080c5bb583f302dfcbcb3c042b6a1
Kiril <102741768+cyrilby@users.noreply.github.com>
2026-05-05 16:42:25 +0200

    Added XLSX export of unmapped messages
    From now on, when running the `data_checks.py` script, the user will get an easy-to-work with XLSX file with the unmapped messages, which can the be copied straight away to the mapping spreadsheet (NB: some consideration about how new rows are handled in the XLSX file and in Google Sheets may create occasional mismatch issues, so check the new statuses manually if they persist as "unmapped" after copying them to the mapping table).

--- data_checks.py
+++ data_checks.py
@@ -1,27 +1,32 @@
-"""
-==================================
-Checking data collection & mapping
-==================================
+# Quick check of CPH metro scraper data
+# Kiril, 12-01-2025
 
-Author: github.com/cyrilby
-Last meaningful update: 03-04-2026
-"""
 
 # %% Setting up
 
-import yaml
 import pandas as pd
-from storage import get_s3_access
 
-# Importing the credentials for working with object storage
-storage_options, bucket = get_s3_access()
+# Setting up access to object storage
+SCW_BUCKET_NAME = "cph-metro-status"
+SCW_ACCESS_KEY = "SCW7853FFRVQ76ZF7YJN"
+SCW_SECRET_KEY = "38f4c08c-7a8b-4446-aadd-8282770fa9b1"
+bucket = SCW_BUCKET_NAME
 
+storage_options = {
+    "key": SCW_ACCESS_KEY,
+    "secret": SCW_SECRET_KEY,
+    "client_kwargs": {
+        "endpoint_url": "https://s3.fr-par.scw.cloud",
+        "region_name": "fr-par",
+    },
+}
 
-# %% Checking data collection status
+# %% Checking data collection
 
 # Getting and previewing raw data
 operation_raw = pd.read_pickle(
-    f"s3://{bucket}/operation_raw.pkl", storage_options=storage_options
+    f"s3://{bucket}/operation_raw.pkl",
+    storage_options=storage_options,
 )
 
 # Replacing any potential NANs with "Unknown"
@@ -31,14 +36,11 @@ print("Showing latest raw data from the CPH metro scraper tool:")
 print(operation_raw.head(20))
 
 
-# %% Checking the mapping completeness
+# %% Checking mapping
 
-# Importing links to mapping tables
-with open("mapping_links.yaml", "r", encoding="utf-8") as file:
-    mapping_links = yaml.safe_load(file)
-
-# Importing user-maintained mapping tables
-mapping_status = pd.read_csv(mapping_links["mapping_status"])
+# Checking for unmapped entries and previewing those
+mapping_link = "https://docs.google.com/spreadsheets/d/1iu7QcYav9865SLSBXGuZWNKUd0ywCrSC0M2vAink1yM/gviz/tq?tqx=out:csv"  # noqa
+mapping_status = pd.read_csv(mapping_link)
 
 unmapped_entries = [
     status
@@ -48,11 +50,15 @@ unmapped_entries = [
 n_unmapped = len(unmapped_entries)
 
 if n_unmapped >= 1:
-    print(f"Note: There are a total of {n_unmapped} unmapped status messages:\n")
+    print(f"\nNote: There are a total of {n_unmapped} unmapped status messages:\n")
     for entry in unmapped_entries:
-        print(entry + "\n")
+        print(entry)
+
+    # Also exporting a local copy of the unmapped entries
+    unmapped_entries = pd.DataFrame({"status_dk": unmapped_entries})
+    unmapped_entries.to_excel("unmapped_entries.xlsx", index=False)
+
 else:
     print("Note: all service status messages have been mapped.")
-
 
 # %%
